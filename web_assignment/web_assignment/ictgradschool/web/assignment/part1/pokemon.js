088804d6b6f743ad565a3c277aeecd9ea8dc5037
CoderYihaoWang <yesiamyihao@gmail.com>
2020-04-16 20:55:11 +1200

    Done part 1

--- web_assignment/web_assignment/ictgradschool/web/assignment/part1/pokemon.js
+++ web_assignment/web_assignment/ictgradschool/web/assignment/part1/pokemon.js
@@ -1,5 +1,12 @@
 /* Base address for the Pokemon endpoints. Add the endpoint name and parameters onto this */
 const ENDPOINT_BASE_URL = "https://sporadic.nz/pokesignment/";
 
+(($) => {
+    // expand and collapse the menu on a smaller screen
+    $("nav img")[0].addEventListener("click", () => {
+        $("nav nav:last-of-type")[0].classList.toggle("expand")
+    })
 
-/* TODO: Your code here */
+
+
+})(document.querySelectorAll.bind(document));
