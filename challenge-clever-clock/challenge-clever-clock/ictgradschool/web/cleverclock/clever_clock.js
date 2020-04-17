ea5433dd5c93fb40a7bc6c76acaabcb5575c8254
CoderYihaoWang <yesiamyihao@gmail.com>
2020-04-17 15:28:29 +1200

    Done

--- challenge-clever-clock/challenge-clever-clock/ictgradschool/web/cleverclock/clever_clock.js
+++ challenge-clever-clock/challenge-clever-clock/ictgradschool/web/cleverclock/clever_clock.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", function () {
-    // helper functions
+    // Clock Task
     const $ = document.getElementById.bind(document)
         , pad = (number) => number.toString().padStart(2, '0')
         , hour = (date) => pad(date.getHours() === 12 ? 12 : date.getHours() % 12)
@@ -10,21 +10,65 @@ window.addEventListener("load", function () {
             ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'][date.getDay()]
         , month = (date) =>
             ['January', 'February', 'March', 'April', 'May', 'June',
-                'July', 'August', 'September', 'October', 'November', 'December'
-            ][date.getMonth()]
+                'July', 'August', 'September', 'October', 'November', 'December'][date.getMonth()]
         , time = (date) => hour(date) + semiColon(date) + minute(date) + ' ' + AMOrPM(date)
-        , day = (date) => `${week(date)}, ${date.getDay()} ${month(date)} ${date.getFullYear()}`
+        , day = (date) => `${week(date)}, ${date.getDate()} ${month(date)} ${date.getFullYear()}`
         , showTime = () => $('clock_face').innerText = time(new Date())
         , showDate = () => $('date_face').innerText = day(new Date())
+        , showClock = () => showTime() && showDate() && showClock
+        , clockTask = () => setInterval(showClock(), 1000);
 
-
-
-
-    // Clock Task
-    showTime(); showDate();
-    setInterval(() => {showTime(); showDate()}, 1000);
+    clockTask();
 
     // Timer Task
+    const AUTO_CLEAR_TIMEOUT = 30000
+        , timer_face = $('timer_face')
+        , run = $('run')
+        , rst = $('rst')
+        , timer = {time: 0, interval: null, timeout: null}
+        , enable = (button) => {
+            button.classList.add('button_enabled');
+            button.classList.remove('button_disabled');
+        }
+        , disable = (button) => button.classList.add('button_disabled')
+        , getTime = (time) => `${Math.floor(time / 10)}.${time % 10}`
+        , updateTime = () => timer_face.innerText = getTime(++timer.time)
+        , resetTime = () => (timer_face.innerText = getTime(timer.time = 0)) && disable(rst)
+        , startTime = () => timer.interval = setInterval(updateTime, 100)
+        , stopTime = () => timer.interval = clearInterval(timer.interval)
+        , startCountdown = () => timer.timeout = setTimeout(resetTime, AUTO_CLEAR_TIMEOUT)
+        , stopCountdown = () => timer.timeout = clearTimeout(timer.timeout);
 
+    function start() {
+        startTime();
+        stopCountdown();
+        disable(rst);
+        run.onclick = stop;
+        run.innerText = 'Stop';
+    }
+
+    function stop() {
+        stopTime();
+        startCountdown();
+        enable(rst);
+        run.onclick = start;
+        run.innerText = 'Start';
+    }
+
+    function reset() {
+        if (timer.time && !timer.interval) {
+            stopCountdown();
+            resetTime();
+        }
+    }
+
+    function timerTask() {
+        enable(run);
+        disable(rst);
+        run.onclick = start;
+        rst.onclick = reset;
+    }
+
+    timerTask();
 
 });
